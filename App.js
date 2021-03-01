ca14792e68064191ddb0be05d7c02b8396fb07b0
Hann Htay <hannhtay@mail.com>
2021-03-01 09:22:45 +0630

    react native pagination

--- App.js
+++ App.js
@@ -1,21 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react'
+import {View, Text, StyleSheet, FlatList, Image, ActivityIndicator} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      data:[],
+      page:1,
+      isLoading: false,
+    }
+  }
+
+  //fetch data
+  componentDidMount(){
+    this.setState({isLoading:true}, this.getData)
+  }
+
+  //get data
+  getData=()=>{
+    console.log(this.state.page+"this is page number")
+    const apiUrl=`https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${this.state.page}`
+    fetch(apiUrl).then((res)=>res.json())
+    .then((resJson)=>{
+      this.setState({
+        data: this.state.data.concat(resJson),
+        isLoading:false
+      }) 
+    })
+  }
+
+  renderItem=({item})=>{
+    return(
+      <View style={styles.itemList}>
+        <Image style={styles.image} source={{ uri:item.thumbnailUrl }} />
+        <Text>
+          {item.title}
+        </Text>
+        <Text>
+          {item.id}
+        </Text>
+      </View>
+    )
+  }
+
+  paginateData=()=>{
+    this.setState({page: this.state.page+1, isLoading:true}, this.getData)
+    console.log(this.state.page)
+  }
+
+  //list footer
+  listFooter=()=>{
+    return(
+      this.state.isLoading?
+      <View style={styles.loading}>
+        
+        <ActivityIndicator size='large' color='red' />
+      </View>:null
+    )
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.data}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index)=>index.toString()}
+          onEndReached={this.paginateData}
+          onEndReachedThreshold={0}
+          ListFooterComponent={this.listFooter}
+         />
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles=StyleSheet.create({
+  container:{
+    paddingTop: 35,
+    paddingHorizontal: 10,
   },
-});
+  itemList: {
+    paddingVertical:10,
+    borderBottomColor: '#eee',
+    borderBottomWidth:1,
+  },
+  image:{
+    width: '100%',
+    height:300,
+  },
+  loading:{
+    marginTop:10,
+    alignItems: 'center',
+  }
+})
+
+export default App;
